7d75ffdad6d4d9a917d8e9cf104a49c89efc320f
kinghalo93 <caioguedes080@gmail.com>
2023-08-31 14:00:03 -0300

    primeira modificação basica

--- client_udp.py
+++ client_udp.py
@@ -1,3 +1,9 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+client.sendto(b'enviando alguma coias  .....', ("localhost", 8080))
+data, sender = client.recvfrom(1024)
+print (sender[0] + ":" + data.decode())
+
