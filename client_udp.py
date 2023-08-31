9a08409a408db01f0a97bd075c7c34e1deed1835
kinghalo93 <caioguedes080@gmail.com>
2023-08-31 14:25:13 -0300

    Algumas pequenas modificações

--- client_udp.py
+++ client_udp.py
@@ -5,12 +5,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     while True:
         
-        mensagem = client.sendto(input("Digite a mensagem aqui .... ").encode(), ("host", 8080))
+        msgem = input("Digite a mensagem aqui ...")
+        client.sendto(msgem.encode(), ("host", 8080))
         data, sender = client.recvfrom(1024)
         print(sender[0] + ":" + data.decode())
 
-        if mensagem.encode ==  "SAIR\n" or sender == "SAIR\n":
-            break    
+        if msgem.encode ==  "SAIR".strip() or sender == "SAIR".strip():
+            break
             
 except Exception as erro:
     print("Alguma coisa está errada: " , erro)
