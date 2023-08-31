c5efe6e44e017ea54aa122a65a014ac661f3e296
KingHalo93 <71747731+guyawaked93@users.noreply.github.com>
2023-08-31 15:18:11 -0300

    Update client_udp.py
    Alteração de mensagem vazia

--- client_udp.py
+++ client_udp.py
@@ -5,19 +5,20 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     while True:
         
-        print("A seguir insira o host e a porta que deseja comincar")
+        print("A seguir insira o host e a porta que deseja comunicar")
         host , porta = input("Digite o host e a porta separado por espaço :").split()
         
         msgem = input("Digite a mensagem aqui ...")
+        if not msgem:
+            print("A mensagem Não Pode estar vazia")
+            continue
+         
         client.sendto(msgem.encode(), (host , int (porta)))
         data, sender = client.recvfrom(1024)
         print(sender[0] + ":" + data.decode())
 
         if msgem.encode().strip() ==  "SAIR" or data.strip() == "SAIR":
             break
-        elif not msgem:
-            print("A mensagem Não Pode estar vazia")
-            continue
             
 except Exception as erro:
     print("Alguma coisa está errada: " , erro)
