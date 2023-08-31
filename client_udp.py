47a4dea534ef015457f70f21ab06052fa9432f55
kinghalo93 <caioguedes080@gmail.com>
2023-08-31 14:14:34 -0300

    Implementaçao de envio e resposta em protocolo udp

--- client_udp.py
+++ client_udp.py
@@ -2,8 +2,19 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+try:
+    while True:
+        
+        mensagem = client.sendto(input("Digite a mensagem aqui .... ").encode(), ("host", 8080))
+        data, sender = client.recvfrom(1024)
+        print(sender[0] + ":" + data.decode())
 
-client.sendto(b'enviando alguma coias  .....', ("localhost", 8080))
-data, sender = client.recvfrom(1024)
-print (sender[0] + ":" + data.decode())
+        if mensagem.encode ==  "SAIR\n" or sender == "SAIR\n":
+            break    
+            
+except Exception as erro:
+    print("Alguma coisa está errada: " , erro)
 
+finally:
+    client.close()
+    
