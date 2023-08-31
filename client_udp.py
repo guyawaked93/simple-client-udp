fc5f4826e13d2fef03b88e70afa9744226d003f0
kinghalo93 <caioguedes080@gmail.com>
2023-08-31 14:42:03 -0300

    Algumas melhorias

--- client_udp.py
+++ client_udp.py
@@ -5,13 +5,19 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
     while True:
         
+        print("A seguir insira o host e a porta que deseja comincar")
+        host , porta = input("Digite o host e a porta separado por espaço :").split()
+        
         msgem = input("Digite a mensagem aqui ...")
-        client.sendto(msgem.encode(), ("host", 8080))
+        client.sendto(msgem.encode(), (host , int (porta)))
         data, sender = client.recvfrom(1024)
         print(sender[0] + ":" + data.decode())
 
-        if msgem.encode ==  "SAIR".strip() or sender == "SAIR".strip():
+        if msgem.encode().strip() ==  "SAIR" or data.strip() == "SAIR":
             break
+        elif not msgem:
+            print("A mensagem Não Pode estar vazia")
+            continue
             
 except Exception as erro:
     print("Alguma coisa está errada: " , erro)
